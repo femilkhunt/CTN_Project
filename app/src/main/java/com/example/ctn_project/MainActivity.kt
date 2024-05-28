82d40b903f9be9cfd426b612f4eca41386534fa7
femilkhuntk <femilkhuntk@gmail.com>
2024-05-28 09:44:42 +0530

    first commit

--- app/src/main/java/com/example/ctn_project/MainActivity.kt
+++ app/src/main/java/com/example/ctn_project/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ctn_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this,home_screen_activity::class.java))
     }
 }
