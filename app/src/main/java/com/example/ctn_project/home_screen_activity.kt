55d2dbbddae4e34080efab735d52296d2703a412
femilkhuntk <femilkhuntk@gmail.com>
2024-05-28 09:41:09 +0530

    first commit

---
+++ app/src/main/java/com/example/ctn_project/home_screen_activity.kt
@@ -0,0 +1,11 @@
+package com.example.ctn_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class home_screen_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
