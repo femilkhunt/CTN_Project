9d99274a0a3653b012bddfde8f0f283b7c2435f1
femilkhuntk <femilkhuntk@gmail.com>
2024-05-28 10:32:10 +0530

    first commit

--- app/src/main/java/com/example/ctn_project/home_screen_activity.kt
+++ app/src/main/java/com/example/ctn_project/home_screen_activity.kt
@@ -1,11 +1,35 @@
 package com.example.ctn_project
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class home_screen_activity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
+
+        var createBtn = findViewById<AppCompatImageView>(R.id.homeScreenAddMoreData)
+
+        createBtn.setOnClickListener(){
+            val Dialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.botton_sheet,null)
+
+            var addAccountBtn = view.findViewById<AppCompatButton>(R.id.BottomSheetAddNewAccount)
+
+            addAccountBtn.setOnClickListener(){
+
+                Dialog.dismiss()
+            }
+            Dialog.setCancelable(false)
+            Dialog.setContentView(view)
+            Dialog.show()
+        }
     }
 }
